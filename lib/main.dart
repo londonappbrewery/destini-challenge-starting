564cb2002e6c787d515e9bc71df99ab6094034f8
Eduardo Rosas <lalorosas@outlook.com>
2020-05-12 12:22:09 -0500

    Display choices
    Complete TODOs 9-14

--- lib/main.dart
+++ lib/main.dart
@@ -59,8 +59,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -81,8 +80,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
